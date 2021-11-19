7dc96cc20ae98d6018e5a3699a14c95f80c794b9
proskuryakoff <misha.proskuryakov.99@gmail.com>
2021-11-19 19:14:51 +0300

    v1

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
+mongoose.connect(
+    "mongodb+srv://proskuryakoff:<password>@main-cluster.qja21.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { 
+        useNewUrlParser: true , 
+        useUnifiedTopology: true  
+    }
+)
+
 const server = express();
 
-server.listen(3000, () => console.log("Server has been started!"))
+server.listen(3000, () => console.log("Server has been started!"));
